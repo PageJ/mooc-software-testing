c7e57183202c316de2186d1da6d629f65b24493e
J Page <i372a@unb.ca>
2018-11-16 18:18:26 -0400

    Create Main for RomanNumeral
    Just for funsies

---
+++ src/main/java/tudelft/roman/RomanNumeralMain.java
@@ -0,0 +1,9 @@
+package tudelft.roman;
+
+public class RomanNumeralMain {
+
+    public static void main (String[] args) {
+        RomanNumeral rn = new RomanNumeral();
+        System.out.println(rn.convert("IX"));
+    }
+}
